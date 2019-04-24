3062ad540c8f76276b6b24caedd472df5d041ba3
Marcelo Alves <marcelo.alves@flexvision.com.br>
2019-04-24 15:42:25 -0300

    Alterações

--- src/main/java/com/example/oauth/keycloaktest/ControllerPages.java
+++ src/main/java/com/example/oauth/keycloaktest/ControllerPages.java
@@ -1,13 +1,9 @@
 package com.example.oauth.keycloaktest;
 
-import java.io.IOException;
 import java.util.Arrays;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,20 +17,10 @@ public class ControllerPages {
 		return "pages/conteudoProtegido";
 	}
 
-	@GetMapping(path = "/login")
-	public String login(HttpServletRequest request) throws ServletException {
-		System.out.println("Teste");
-		return "/";
-	}
-
 	@GetMapping(path = "/sair")
-	public void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		new SecurityContextLogoutHandler().logout(request, null, null);
-        try {
-            response.sendRedirect(request.getHeader("referer"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	public String logout(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		request.logout();
+		return "/";
 	}
 
 }
--- src/main/java/com/example/oauth/keycloaktest/SecurityConf.java
+++ src/main/java/com/example/oauth/keycloaktest/SecurityConf.java
@@ -3,26 +3,14 @@ package com.example.oauth.keycloaktest;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
 @EnableOAuth2Sso
 @EnableWebSecurity
 public class SecurityConf extends WebSecurityConfigurerAdapter {
-	/*
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-	        .withUser("graxa").password("1234").roles("USER").and()
-	        .withUser("admin").password("admin").roles("ADMIN");
-    }
-    */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,13 +23,6 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
   		http.logout()
            .logoutSuccessUrl("http://localhost:8080/auth/realms/dev/protocol/openid-connect/logout");
     }
-
-    /*
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-    	return NoOpPasswordEncoder.getInstance();
-    }
-    */
     
     @Bean
     public CustomFilter customFilter() {
